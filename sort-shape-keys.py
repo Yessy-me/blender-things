093b728d1981e7c1b0a7793de9a81d45f59cfd9e
Yessy-me <66486609+Yessy-me@users.noreply.github.com>
2020-06-05 19:49:59 +0300

    Create sort-shape-keys.py
    Sorts shape keys in alphabetical order on a selected mesh

---
+++ sort-shape-keys.py
@@ -0,0 +1,12 @@
+import bpy
+
+ob = bpy.context.object
+skeys = ob.data.shape_keys.key_blocks 
+
+skey_names = sorted(skeys.keys(), key=lambda v: v.upper())
+
+for name in skey_names:
+   if name != 'basis':
+      idx = skeys.keys().index(name)
+      ob.active_shape_key_index = idx
+      bpy.ops.object.shape_key_move(type='BOTTOM')
