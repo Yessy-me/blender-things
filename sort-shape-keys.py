4a59ee5fdfbeca044725ef4622e8ed16cc6cb8ff
Yessy-me <66486609+Yessy-me@users.noreply.github.com>
2020-06-05 20:14:19 +0300

    Update sort-shape-keys.py

--- sort-shape-keys.py
+++ sort-shape-keys.py
@@ -6,7 +6,7 @@ skeys = ob.data.shape_keys.key_blocks
 skey_names = sorted(skeys.keys(), key=lambda v: v.upper())
 
 for name in skey_names:
-   if name != 'basis':
+   if name.lower() != 'basis':
       idx = skeys.keys().index(name)
       ob.active_shape_key_index = idx
       bpy.ops.object.shape_key_move(type='BOTTOM')
